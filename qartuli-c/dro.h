85f1d7216cd60782e67fd49dd3b4e1be3f3fa0e6
X1nto <burtkni@yandex.ru>
2025-01-23 01:15:44 +0400

    add initial dro

--- qartuli-c/dro.h
+++ qartuli-c/dro.h
@@ -8,8 +8,14 @@
 #include <time.h>
 #include "qartuli.h"
 
-ჩასმული void test() {
+typedef  clock_t
 
-}
+char *asctime(const struct tm *timeptr);
+clock_t clock(void)
+char *ctime(const time_t *timer)
+double difftime(time_t time1, time_t time2);
+struct tm *localtime(const time_t *timer);
+time_t mktime(struct tm *timeptr);
+size_t strftime(char *str, size_t maxsize, const char *format, const struct tm *timeptr);
 
 #endif //DRO_H
